d882f5cbafba38e29bc8ccb48cc6de66e7689414
neuronzzz <zhangyuspirit@gmail.com>
2024-03-31 11:28:56 +0800

    add qcc example with login feature

--- README.md
+++ README.md
@@ -7,6 +7,11 @@
 * qcc code
   * Python爬企查查网站数据的爬虫代码如何写？ - 张同学的回答 - 知乎
     * https://www.zhihu.com/question/46234054/answer/2925292345
+      * todos
+        * 如何正确的找到element的XPATH
+          * 超过一定时间，提示再登录
+        * 打开新的标签页，需要切换新的标签页
+        * 拿到信息之后，关闭标签页
   * tbd
     * django issue
       * https://stackoverflow.com/questions/70319606/importerror-cannot-import-name-url-from-django-conf-urls-after-upgrading-to
--- labs/qcc.py
+++ labs/qcc.py
@@ -39,6 +39,9 @@ def get_inc_list():
 inc_list = ["阿里巴巴", "字节跳动"]
 inc_len = len(inc_list)
 
+credit_list = []  # 用来装社会信用代码的空列表
+capital_list = []
+
 # 开始爬虫
 for i in range(inc_len):
     txt = inc_list[i]
@@ -60,21 +63,54 @@ for i in range(inc_len):
     srh_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div/div[1]/div/div/div/div/span/button')
     srh_btn.click()
 
-Credit_List = []  # 用来装社会信用代码的空列表
-try:
-    # 直接找到装在社会信用代码的标签
-    credit_code = driver.find_element(By.XPATH,
-                                      '/html/body/div[1]/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div/div[3]/div[1]/span[4]/span').text
+    time.sleep(2)
+    btnn = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div[2]/span/span[1]/a')
+    btnn.click()
+    time.sleep(2)
 
-    # 字符化，防止后续出现编码问题
-    credit_code = str(credit_code)
-    print(credit_code, "社会码")  # 每找到一条，则打印一条在终端上。
-    Credit_List.append(credit_code)  # 添加到列表
-except:
-    credit_code = '没有找到社会码,可能是企业名称非全称导致筛选信息模糊...'
-    Credit_List.append(credit_code)  # 没找到也要添加到列表里面去，实现与企业一一对应
-    print("没有找到社会码,可能是企业名称非全称导致筛选信息模糊...")
+    try:
+        # 社会码
+        # /html/body/div[1]/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div/div[3]/div[1]/span[4]/span
+        # //*[@id="cominfo"]/div[2]/table/tr[1]/td[2]/span/span[1]
+        # //*[@id="cominfo"]/div[2]/table/tr[4]/td[6]/span/span[1]
+        # /html/body/div[1]/div[2]/div[5]/div[2]/div/div[2]/section[2]/div[2]/table/tr[1]/td[2]/span/span[1]
+        # /html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[1]/span/span/span/span[1]
+        # /html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]
 
+        # 注册资本
+        # /html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]
+
+        # 直接找到装在社会信用代码的标签
+        credit_code = driver.find_element(By.XPATH,
+                                          '//*[@id="cominfo"]/div[2]/table/tr[4]/td[2]/span/span[1]').text
+                                          # '/html/body/div/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div/div[3]/div[1]/span[2]/span').text
+                                          # '/html/body/div/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div/span/span[1]/a/span').text
+                                          # '/html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]').text
+                                          # '/html/body/div[1]/div[2]/div[2]/div[3]/div/div[2]/div/table/tr[1]/td[3]/div/div[3]/div[1]/span[4]/span').text
+        # 字符化，防止后续出现编码问题
+        credit_code = str(credit_code)
+        print(credit_code, "社会码")  # 每找到一条，则打印一条在终端上。
+        credit_list.append(credit_code)  # 添加到列表
+
+        # /html/body/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]
+        # /html/body/div/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]
+
+        capital = driver.find_element(By.XPATH,
+                                      '/html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div/div/div/div[1]/div[1]/div[3]/span/span[2]').text
+
+        capital = str(capital)
+        print(capital, "注册资本")
+        capital_list.append(capital)
+    except:
+        credit_code = '没有找到社会码,可能是企业名称非全称导致筛选信息模糊...'
+        credit_list.append(credit_code)  # 没找到也要添加到列表里面去，实现与企业一一对应
+        print("没有找到社会码,可能是企业名称非全称导致筛选信息模糊...")
+
+        capital = '没有找到注册资本,可能是企业名称非全称导致筛选信息模糊...'
+        capital_list.append(capital)  # 没找到也要添加到列表里面去，实现与企业一一对应
+        print("没有找到注册资本,可能是企业名称非全称导致筛选信息模糊...")
+
+time.sleep(1000)
 
 # 读取到的数据存入excel中
 def save_to_excel():
@@ -83,10 +119,10 @@ def save_to_excel():
     wb = pl.load_workbook(r'爬虫结果.xlsx')  # 打开工作簿
     ws = wb.active  # 读取第一张工作表
     i = 0
-    while i < len(Credit_List):
+    while i < len(credit_list):
         ws.cell(row=i + 1, column=2,
-                value=Credit_List[i])  # 将信用代码的列表中的每个数据写入到excel中，cell(row=1,column=1,values = 10) 表示A1单元格内容是10.
-        print(f'{Credit_List[i]}信用码正在存入{ws.cell(i + 1, 1).value}中')  # cell(1,1).value表示单元格A1的值
+                value=credit_list[i])  # 将信用代码的列表中的每个数据写入到excel中，cell(row=1,column=1,values = 10) 表示A1单元格内容是10.
+        print(f'{credit_list[i]}信用码正在存入{ws.cell(i + 1, 1).value}中')  # cell(1,1).value表示单元格A1的值
         i = i + 1
     wb.save(r'爬虫结果.xlsx')  # 保存文件，这一步必须需要，否则文件没保存等于白搞。
     print("Already get things done ,please check the file now!")  # 可以自行编辑程序完成之后的输出内容。
